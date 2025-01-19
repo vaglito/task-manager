7aec5e8fa0ae07e8de1d658034ed2983aa14d4d0
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-01-18 19:42:37 -0500

    feat: Change the validations of the Dto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,16 @@
+import {
+  IsString,
+  IsBoolean,
+} from 'class-validator';
+
 export class CreateTaskDto {
+
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsBoolean()
   done: boolean;
 }
