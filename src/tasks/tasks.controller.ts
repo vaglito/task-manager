c6feb710f122660dff430cc3a41a0de39fc0840f
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-02-19 20:02:14 -0500

    feat: add validation on controllers

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,41 +1,66 @@
-import { Controller, Get, Post, Put, Delete, Patch, Body, Query, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Patch,
+  Body,
+  NotFoundException,
+  BadRequestException,
+  Param,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Tasks } from '@prisma/client';
 
 @Controller('/tasks')
 export class TasksController {
+  constructor(private tasksServices: TasksService) {}
 
-    constructor(private tasksServices:TasksService) {}
+  @Get()
+  async getAllTasks() {
+    return this.tasksServices.getAllTasks();
+  }
 
-    @Get()
-    async getAllTasks() {
-        return this.tasksServices.getAllTasks();
+  @Get(':id')
+  async getTaskById(@Param('id') id: string) {
+    const taskFound = await this.tasksServices.getTaskById(id);
+    if (!taskFound) {
+      throw new NotFoundException('Tarea no encontrada');
     }
+    return taskFound;
+  }
 
-    @Get(':id')
-    async getTaskById(@Param('id') id: string) {
-        return this.tasksServices.getTaskById(id);
+  @Post('')
+  async createTasks(@Body() data: Tasks) {
+    try {
+        return await this.tasksServices.createTask(data);
     }
-
-    @Post('')
-    async createTasks(@Body() data: Tasks) {
-        return this.tasksServices.createTask(data);
+    catch (error) {
+      throw new BadRequestException('No se proporcionaron los datos necesarios');
     }
+  }
 
-    @Put(':id')
-    async updateTasks(@Param('id') id: string, @Body() data: Tasks) {
-        return this.tasksServices.updateTask(id, data);
+  @Put(':id')
+  async updateTasks(@Param('id') id: string, @Body() data: Tasks) {
+    try {
+        return await this.tasksServices.updateTask(id, data);
+    } catch (error) {
+      throw new NotFoundException('Tarea no encontrada');
     }
+  }
 
-    @Delete('id')
-    async deleteTasks(@Param('id') id: string) {
-        return this.tasksServices.deleteTask(id);
+  @Delete(':id')
+  async deleteTasks(@Param('id') id: string) {
+    try {
+      return await this.tasksServices.deleteTask(id);
+    } catch (error) {
+      throw new NotFoundException('Tarea no encontrada');
     }
+  }
 
-    @Patch()
-    patchTasks() {
-        return this.tasksServices.patchTask();
-    }
-
-
+  @Patch()
+  patchTasks() {
+    return this.tasksServices.patchTask();
+  }
 }
